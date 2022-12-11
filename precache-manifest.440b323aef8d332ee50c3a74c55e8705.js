090ebd8326d5f590a19aa5f607fa0e0393e42eb8
MatiasOjala <matias982@gmail.com>
2022-12-12 01:40:05 +0200

    Updates

---
+++ precache-manifest.440b323aef8d332ee50c3a74c55e8705.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "6390997e25ca44ba3193",
+    "url": "/MatiasOjala/Personaltrainerdeploy.git/static/css/main.cbd2fe97.chunk.css"
+  },
+  {
+    "revision": "6390997e25ca44ba3193",
+    "url": "/MatiasOjala/Personaltrainerdeploy.git/static/js/main.6390997e.chunk.js"
+  },
+  {
+    "revision": "b7acc65bb88060d5f158",
+    "url": "/MatiasOjala/Personaltrainerdeploy.git/static/js/1.b7acc65b.chunk.js"
+  },
+  {
+    "revision": "0487f2ef762155732c44",
+    "url": "/MatiasOjala/Personaltrainerdeploy.git/static/css/2.a0806ba5.chunk.css"
+  },
+  {
+    "revision": "0487f2ef762155732c44",
+    "url": "/MatiasOjala/Personaltrainerdeploy.git/static/js/2.0487f2ef.chunk.js"
+  },
+  {
+    "revision": "972a0eb54009a64862d7",
+    "url": "/MatiasOjala/Personaltrainerdeploy.git/static/js/runtime~main.972a0eb5.js"
+  },
+  {
+    "revision": "5d9cd668455eb872164b1d5a72542618",
+    "url": "/MatiasOjala/Personaltrainerdeploy.git/static/media/TrainingsCalendar.5d9cd668.bin"
+  },
+  {
+    "revision": "b2351b2f707c7d11ebccf544edba82b1",
+    "url": "/MatiasOjala/Personaltrainerdeploy.git/index.html"
+  }
+];
